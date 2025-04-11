ac129a86047faeff0d52b7d698fce54f96f4ca32
jhfg55-DevBoxVer <193506841+jhfg55-DevBoxVer@users.noreply.github.com>
2025-04-11 20:58:28 +0800

    新增基准测试类和方法
    添加了 `AotUncachedValueListBenchmarks` 基准测试类，配置了作业和参数。新增的 `ValueList_AOT_NonArena_Benchmarks` 类包含 `AddElements` 和 `ClearList` 方法，用于测试 `ValueList<int>` 的性能，并实现了全局设置和清理方法以管理资源。

---
+++ src/NullGC.DragRace/Benchmarks/AotUncachedValueListBenchmarks.cs
@@ -0,0 +1,67 @@
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.NativeAot;
+using NullGC.Allocators;
+using NullGC.Collections;
+using NullGC.TestCommons;
+
+namespace NullGC.DragRace.Benchmarks
+{
+    // 定义一个基于 NativeAOT 的配置
+    public class AotUncachedValueListBenchmarks : ManualConfig
+    {
+        public AotUncachedValueListBenchmarks()
+        {
+            AddJob(Job.Default
+                .WithToolchain(NativeAotToolchain.Instance)
+                .WithWarmupCount(1)
+                .WithIterationCount(5));
+        }
+    }
+
+    [Config(typeof(AotUncachedValueListBenchmarks))]
+    public class ValueList_AOT_NonArena_Benchmarks
+    {
+        private ValueList<int> _valList;
+        private int _count = 1_000_000;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            // 使用非 Arena 模式（uncached/unscoped）的内存分配配置
+            AllocatorContextInitializer.SetupDefaultUncachedUnscopedAllocationContext(out IMemoryAllocationTrackable? allocTracker,
+                out IMemoryAllocationTrackable? nativeTracker);
+            // 创建一个初始容量为0的 ValueList<int>
+            _valList = new ValueList<int>(0);
+        }
+
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            _valList.Clear();
+        }
+
+        [Benchmark]
+        public void AddElements()
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                _valList.Add(i);
+            }
+        }
+
+        [Benchmark]
+        public void ClearList()
+        {
+            _valList.Clear();
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            _valList.Dispose();
+            AllocatorContext.ClearProvidersAndAllocations();
+        }
+    }
+}
